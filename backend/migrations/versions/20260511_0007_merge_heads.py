7d35663c244226f6fa6880fa6e7d1f7eeb79308c
halidhocaoglu <halidhocaoglu@users.noreply.github.com>
2026-05-11 16:24:45 +0300

    Merge Alembic migration heads

---
+++ backend/migrations/versions/20260511_0007_merge_heads.py
@@ -0,0 +1,19 @@
+"""merge social graph and users is_active heads
+
+Revision ID: 20260511_0007
+Revises: 20260510_0003, 20260511_0006
+Create Date: 2026-05-11 00:30:00.000000
+"""
+
+revision = "20260511_0007"
+down_revision = ("20260510_0003", "20260511_0006")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
